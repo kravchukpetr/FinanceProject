a3a471bb3e5fa8b4766754167cddd01c1b255f65
Kravchuk Petr <kravchukpetr@gmail.com>
2021-02-21 17:58:49 +0300

    . Signed-off-by: Kravchuk Petr <kravchukpetr@gmail.com>

--- RunDailyFinance.py
+++ RunDailyFinance.py
@@ -5,6 +5,6 @@ import os
 # print('Parameters:')
 # print(sys.argv[0] + ' ' + sys.argv[1] + ' ' + sys.argv[2] + ' ' + sys.argv[3] + ' ' + sys.argv[4])
 
-fname = os.path.dirname(os.path.realpath(__file__)).replace("\\","/") + '/' + 'config.txt'
+fname = os.path.dirname(os.path.realpath(__file__)).replace("\\","/") + '/' + 'DB.config'
 log_dir = os.path.dirname(os.path.realpath(__file__)).replace("\\","/")
 fl.DailyUpdateQuote(fname, log_dir)
