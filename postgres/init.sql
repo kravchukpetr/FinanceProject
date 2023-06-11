7d8a88d3ad0ff8224f6aff92f07f73b5d49174f2
Kravchuk Petr <p.kravchuk@ddl.com>
2023-06-12 00:24:13 +0200

    .

--- postgres/init.sql
+++ postgres/init.sql
@@ -5,11 +5,11 @@ create table if not exists  finance.Quotes
 (
 Dt date,
 Stock varchar(10),
-OpenValue numeric(20,6),	
-HighValue numeric(20,6),	
+OpenValue numeric(20,6),
+HighValue numeric(20,6),
 LowValue  numeric(20,6),
-CloseValue numeric(20,6),	
-AdjClose numeric(20,6),	
+CloseValue numeric(20,6),
+AdjClose numeric(20,6),
 Volume numeric(20,6),
 LoadDt timestamp default now(),
 primary key (Dt, Stock)
@@ -20,7 +20,7 @@ create table if not exists  finance.Stock
 (
 Stock varchar(10),
 Security varchar(300),
-Sector varchar(100),	
+Sector varchar(100),
 SubIndustry  varchar(100),
 Market varchar(50),
 LoadDt timestamp default now(),
@@ -34,8 +34,8 @@ drop table if exists wf.Stock;
 create table if not exists wf.WorkFlowLogs
 (
 ID serial,
-WF_ID int, 
-WF_STATUS int, 
+WF_ID int,
+WF_STATUS int,
 CNT_ERRORS int,
 LoadDt timestamp default now(),
 EndDt timestamp,
@@ -46,12 +46,10 @@ primary key (ID)
 drop table if exists wf.WorkFlows;
 create table  if not exists wf.WorkFlows
 (
-WF_ID serial, 
-WF_SC VARCHAR(100), 
-WF_NM VARCHAR(500), 
+WF_ID serial,
+WF_SC VARCHAR(100),
+WF_NM VARCHAR(500),
 primary key (WF_ID)
 );
 
-INSERT INTO  wf.WorkFlows (WF_SC, WF_NM) SELECT 'FinanceDailyLoad', 'Ежедневная загрузка данных по котировкам';
-
-
+INSERT INTO  wf.WorkFlows (WF_SC, WF_NM) SELECT 'FinanceDailyLoad', 'Р•Р¶РµРґРЅРµРІРЅР°СЏ Р·Р°РіСЂСѓР·РєР° РґР°РЅРЅС‹С… РїРѕ РєРѕС‚РёСЂРѕРІРєР°Рј';
