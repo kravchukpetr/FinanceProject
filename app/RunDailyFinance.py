733aafdf9b02c3ef9e065c691afcb920efb7aefc
Petr Kravchuk <p.kravchuk@ddl.com>
2024-08-06 11:54:45 +0200

    fix env

--- app/RunDailyFinance.py
+++ app/RunDailyFinance.py
@@ -2,6 +2,8 @@ import FinanceLib as fl
 import MaintenanceLib as mntl
 import warnings
 warnings.filterwarnings('ignore')
+from dotenv import load_dotenv
+load_dotenv()
 
 fl.daily_update_quote()
 fl.get_all_stock_recomendation()
--- python/run_simfin.sh
+++ python/run_simfin.sh
@@ -1,1 +1,2 @@
+export $(grep -v '^#' /usr/src/app/.env | xargs)
 /usr/local/bin/python /usr/src/app/RunDailySimfin.py
